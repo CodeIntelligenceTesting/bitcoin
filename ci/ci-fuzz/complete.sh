a703b61087a6bf31d6b2b645cc5f8b460536ca78
janschrewe <schrewe@uni-bonn.de>
2020-12-15 12:43:48 +0100

    Fix shell script syntax error

--- ci/ci-fuzz/complete.sh
+++ ci/ci-fuzz/complete.sh
@@ -39,7 +39,7 @@ if [ ! -f "${CICTL}" ]; then
  wget "${CICTL_URL}" -O "${CICTL}"
 fi
 # Verify the checksum
-echo "${CICTL_SHA256SUM} ${CICTL} | sha256sum --check
+echo "${CICTL_SHA256SUM} ${CICTL}" | sha256sum --check
 
 # Make it executable
 chmod +x "${CICTL}"
