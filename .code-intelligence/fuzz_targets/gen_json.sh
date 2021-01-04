1973d6a9b2c5f0b234f0ff2644480610d83b8b85
janschrewe <schrewe@uni-bonn.de>
2021-01-04 13:48:19 +0100

    Use buildflags.json instead of template.cpp.json
    The build flags for the generated JSON files are now specified in
    buildflags.json, which is a simple JSON file only containing the build
    flags

--- .code-intelligence/fuzz_targets/gen_json.sh
+++ .code-intelligence/fuzz_targets/gen_json.sh
@@ -1,16 +1,17 @@
 #!/bin/sh
+export LC_ALL=C
 
 #this script generates a .json config file for each *.cpp file in the directory. 
-#It copies the buildFlags from template.cpp.json. So to update build flags just change them in the template and (re-)run this script
+#It copies the buildFlags from buildflags.json. So to update build flags just change them in the template and (re-)run this script
 
 DIRECTORY=.
 
 for file in $DIRECTORY/*.cpp; do
-    filename=${file##*/}
-    test_name=${filename%.*}
-    parent_path=".code-intelligence/fuzz_targets/"
+    filename=${file##*/} #this is the name of the C++ file without the starting "./", for example block.cpp
+    test_name=${filename%.*} #this is the name of the test, the file extension is removed for this: block.cpp -> block
+    parent_path=".code-intelligence/fuzz_targets/" #the path of the fuzz targets must be hardcoded in the JSON file, relative to project root
     api="${parent_path}${filename}"
-    json_file_name=${file}.json
+    json_file_name=${file}.json #name of the newly generated json file. Append .json to the name of the C++ file.
     echo "Generating $json_file_name"
-    jq --arg t_name $test_name --arg api $api  '{name: $t_name,displayName: $t_name,buildFlags: .buildFlags,cApi: {api: {relativePath: $api}}}' template.cpp.json > $json_file_name
+    jq --arg t_name $test_name --arg api $api  '{name: $t_name,displayName: $t_name,buildFlags: .buildFlags,cApi: {api: {relativePath: $api}}}' buildflags.json > $json_file_name
 done
