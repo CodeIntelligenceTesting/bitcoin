5ebff5600d1438d8a18cd298722e8a154fdd51c8
janschrewe <schrewe@uni-bonn.de>
2021-01-04 15:20:40 +0100

    Add campaign json file configuration
    For every fuzz target a campaign json file is created using the default
    configuration from .code-intelligence/campaigns/options.json

--- .code-intelligence/fuzz_targets/gen_json.sh
+++ .code-intelligence/fuzz_targets/gen_json.sh
@@ -6,12 +6,16 @@ export LC_ALL=C
 
 DIRECTORY=.
 
-for file in $DIRECTORY/*.cpp; do
+for file in "$DIRECTORY"/*.cpp; do
     filename=${file##*/} #this is the name of the C++ file without the starting "./", for example block.cpp
     test_name=${filename%.*} #this is the name of the test, the file extension is removed for this: block.cpp -> block
     parent_path=".code-intelligence/fuzz_targets/" #the path of the fuzz targets must be hardcoded in the JSON file, relative to project root
     api="${parent_path}${filename}"
     json_file_name=${file}.json #name of the newly generated json file. Append .json to the name of the C++ file.
+    campaign_file_name=../campaigns/$test_name.json
     echo "Generating $json_file_name"
-    jq --arg t_name $test_name --arg api $api  '{name: $t_name,displayName: $t_name,buildFlags: .buildFlags,cApi: {api: {relativePath: $api}}}' buildflags.json > $json_file_name
+    jq -j --arg t_name $test_name --arg api $api  '{name: $t_name,displayName: $t_name,buildFlags: .buildFlags,cApi: {api: {relativePath: $api}}}' buildflags.json > $json_file_name
+    
+    #create config files for campaigns
+    jq -j --arg t_name $test_name '{name: $t_name,displayName: $t_name,maxRunTime: .maxRunTime,fuzzTargets: $t_name,fuzzerRunConfigurations: .fuzzerRunConfigurations}' ../campaigns/options.json > $campaign_file_name
 done
