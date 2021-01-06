43d9d0ff53ff8234d290da79e1e7555d0f242c9e
janschrewe <schrewe@uni-bonn.de>
2021-01-06 15:18:44 +0100

    Add more comments to cicd_scripts_template

--- .code-intelligence/cicd_script_template.sh
+++ .code-intelligence/cicd_script_template.sh
@@ -3,14 +3,12 @@ export LC_ALL=C
 # Display name of the project.
 PROJECT='bitcoin'
 # Display name of the campaign to be run.
-CAMPAIGN='TARGET_NAME'
+CAMPAIGN='addrman'
 # Address of the fuzzing service
 FUZZING_SERVICE_URL='grpc-api.demo.code-intelligence.com:443'
 # Address of the fuzzing web interface
 WEB_APP_URL='app.demo.code-intelligence.com'
 
-# Credentials for accessing the fuzzing service
-#CIFUZZ_CREDS=credentials('CIFUZZ_CREDS')
 CICTL='cictl-2.16.3-389-gf8e52f113-linux';
 CICTL_VERSION='2.16.3-389-gf8e52f113';
 CICTL_SHA256SUM='e89234fe8a5c7c7b336815f975de7faf76a3420a40c51a7b8f396372f89ab9c1';
@@ -18,7 +16,7 @@ CICTL_URL='https://s3.eu-central-1.amazonaws.com/public.code-intelligence.com/ci
 FINDINGS_TYPE='CRASH';
 TIMEOUT='180'
 #Email that will receive reports if any finding is encountered.
-#CI_FUZZING_REPORT_EMAIL_RECIPIENT='schrewe@code-intelligence.com'
+#CI_FUZZING_REPORT_EMAIL_RECIPIENT='user@example.com'
 GIT_BRANCH='master'
 
 export PROJECT
@@ -47,16 +45,16 @@ chmod +x "${CICTL}"
 
 set -eu
 # Log in
-#./${CICTL} login -u "${CIFUZZ_CREDS_USR}" -p "${CIFUZZ_CREDS_PSW}"
+#CIFUZZ_TOKEN is set as "encrypted value" in the travis
 ./${CICTL} login -t "${CIFUZZ_TOKEN}"
 # Start Fuzzing
-LOG_FILE="start-$(basename "TARGET_NAME").logs"
+LOG_FILE="start-$(basename "addrman").logs"
 
 ./${CICTL} start_fuzzing --daemon_listen_address="${FUZZING_SERVICE_URL}" --project_name="${PROJECT}" --campaign_name="${CAMPAIGN}" --git_branch="${GIT_BRANCH#*/}" | tee "${LOG_FILE}"
 
 set -eu
 # Get the name of the started campaign run from the logs
-LOG_FILE="start-$(basename "TARGET_NAME").logs"
+LOG_FILE="start-$(basename "addrman").logs"
 LINE=$(grep "display_name:" "${LOG_FILE}")
 CAMPAIGN_RUN=${LINE#*"display_name: "}
 
